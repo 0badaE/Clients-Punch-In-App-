2e1de182c8abb61330b72be5e18ad7c466836ba5
Obi.É <105763414+0badaE@users.noreply.github.com>
2024-02-15 05:13:31 -0500

    made some changes to photo links

--- src/components/LogIn/Signup.jsx
+++ src/components/LogIn/Signup.jsx
@@ -58,7 +58,7 @@ function Signup(props){
 
                     <button onClick = {signUpWithGoogle} className="google-login-btn">
                         
-                    <img className = "google-Logo" src="./src/photos/google-logo.png"/>Sign Up with Google</button>
+                    <img className = "google-Logo" src="https://png2.cleanpng.com/sh/4e05052fc2fd3c145566129da3067597/L0KzQYm3VMI0N6V7fZH0aYP2gLBuTfdwd5hxfZ91b3fyPb32hB8udJDsh58AYXToR7XqV8g1bWNmSJC9MEa7R4G3WME2OmU6S6IBM0G4RIW6TwBvbz==/kisspng-google-logo-logo-logo-5ade7dc784e2a0.4068700815245306315443.png"/>Sign Up with Google</button>
                 </div>
             </div>
         </div>
