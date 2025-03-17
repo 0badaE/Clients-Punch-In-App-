d823d2adeba00233f574077d433fe9f7c52c9059
Obi.É <Obada.Erfan97@gmail.com>
2025-03-17 00:34:23 -0400

    first commit

--- src/components/LogIn/ForgotPassword.jsx
+++ src/components/LogIn/ForgotPassword.jsx
@@ -1,34 +1,34 @@
 import React from "react"
-import { Link,useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
- 
-function ForgotPassword(props){
-const [email,setEmail] = React.useState("")
-const [sent,setSent] = React.useState(null)
-const [failed,setFailed] = React.useState(false)
-const auth = getAuth();
-const navigate = useNavigate();
 
-async function sendResetEmail(){
+function ForgotPassword(props) {
+  const [email, setEmail] = React.useState("")
+  const [sent, setSent] = React.useState(null)
+  const [failed, setFailed] = React.useState(false)
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  async function sendResetEmail() {
     await sendPasswordResetEmail(auth, email)
       .then(setSent(true))
-      .then(setTimeout(function(){
+      .then(setTimeout(function () {
         navigate("/")
-    }, 1000))
+      }, 1000))
       .catch((error) => {
         console.error(error)
         setFailed(true)
       });
-}
-    return(
-        <div className="login-page">
-            <Link className="backButton forgotpass" to="/"> &larr; Sign In</Link>
-            <input className = "loginInfo" type="email" placeholder="Email" onChange = {(e)=> setEmail(e.target.value)}/>
-            <p style={{color: "rgb(102 183 104)"}}>{sent ? "Password Reset Email Sent" : ""}</p>
-            <p style={{color: "rgb(201 50 50)"}}>{failed ? "Please Try Again" : ""}</p>
-            <button onClick={sendResetEmail}>Reset Password</button>
-        </div>
-    )
+  }
+  return (
+    <div className="login-page">
+      <Link className="backButton forgotpass" to="/"> &larr; Sign In</Link>
+      <input className="loginInfo" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <p style={{ color: "rgb(102 183 104)" }}>{sent ? "Password Reset Email Sent" : ""}</p>
+      <p style={{ color: "rgb(201 50 50)" }}>{failed ? "Please Try Again" : ""}</p>
+      <button onClick={sendResetEmail}>Reset Password</button>
+    </div>
+  )
 }
 
 export default ForgotPassword
